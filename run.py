43e2eea9a8e582870e0a769099f23ffac415acb4
email2ify <110549305+email2ify@users.noreply.github.com>
2022-12-05 09:07:48 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,273 +1,135 @@
-# ------- Global Variables -------
+import gspread
+from google.oauth2.service_account import Credentials
 
-# Our New Game Board
-board = ["*", "*", "*",
-         "*", "*", "*",
-         "*", "*", "*"]
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-# Game in progress / finsihed
-ongoing_game = True
-
-# Game winner
-winner = None
-
-# Current Player (X starts first)
-current_player = "X"
-
-# ------- Classes -------
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-
-class SpaceTakenError(Error):
-    """Raised when the input space is already taken"""
-    pass
-
-# ------- Functions -------
-
-
-def y_n_prompt():
-    """ Prompts the user with a Y/N question
-    to play another game
+def get_sales_data():
     """
-    start = input("Would you like to play a game? Y/N: ").upper()
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
     while True:
-        try:
-            if start != "Y":
-                if start != "N":
-                    raise ValueError
-        except ValueError:
-            print("\n")
-            print("Invalid character, Please try again")
-            y_n_prompt()
-        else:
-            if start.upper() == "Y":
-                play_game()
-            elif start.upper() == "N":
-                print("\n")
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-                print("Find me on GitHub, TechCentreUK")
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-                print("\n")
-                exit()
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here:\n")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 
-def play_again():
-    print("\n")
-    """ Ask user for another game,
-    if yes resets board
-    and if no exits out
+def validate_data(values):
     """
-    play = input("Would you like to play another game? Y/N: ").upper()
-    while True:
-        try:
-            if play != "Y":
-                if play != "N":
-                    raise ValueError
-        except ValueError:
-            print("\n")
-            print("Invalid character, Please try again")
-            y_n_prompt()
-        else:
-            if play.upper() == "Y":
-                global board
-                board = ["*", "*", "*", "*", "*", "*", "*", "*", "*"]
-                global ongoing_game
-                ongoing_game = True
-                global current_player
-                current_player = "X"
-                global winner
-                winner = None
-                play_game()
-            elif play.upper() == "N":
-                print("\n")
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-                print("Find me on GitHub, TechCentreUK")
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-                print("\n")
-                exit()
-
-
-def swap_player():
-    """ Swap the current player from X to O, or O to X """
-    global current_player
-    if current_player == "X":
-        current_player = "O"
-    elif current_player == "O":
-        current_player = "X"
-
-
-def check_for_tie():
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
-    Check for a winner and if any spaces left
-    if no winner and no spaces then returns a tie
-    """
-    global ongoing_game
-    check_for_winner()
-    if "*" not in board and winner is None:
-        ongoing_game = False
-        print("Game is a Tie! \n")
-        play_again()
-        return True
-    else:
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
         return False
 
+    return True
 
-def check_diagonals():
+
+def update_worksheet(data, worksheet):
     """
-    Check all diagonal rows for a winner
-    if no winner then returns None
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
     """
-    global ongoing_game
-    diagonal_1 = board[0] == board[4] == board[8] != "*"
-    diagonal_2 = board[2] == board[4] == board[6] != "*"
-    if diagonal_1 or diagonal_2:
-        ongoing_game = False
-    if diagonal_1:
-        return board[0]
-    elif diagonal_2:
-        return board[2]
-    else:
-        return None
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
-def check_columns():
+def calculate_surplus_data(sales_row):
     """
-    Check all columns for a winner
-    if no winner then returns None
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
-    global ongoing_game
-    column_1 = board[0] == board[3] == board[6] != "*"
-    column_2 = board[1] == board[4] == board[7] != "*"
-    column_3 = board[2] == board[5] == board[8] != "*"
-    if column_1 or column_2 or column_3:
-        ongoing_game = False
-    if column_1:
-        return board[0]
-    elif column_2:
-        return board[1]
-    elif column_3:
-        return board[2]
-    else:
-        return None
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 
-def check_rows():
+def get_last_5_entries_sales():
     """
-    Check all rows for a winner
-    if no winner then returns None
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
     """
-    global ongoing_game
-    row_1 = board[0] == board[1] == board[2] != "*"
-    row_2 = board[3] == board[4] == board[5] != "*"
-    row_3 = board[6] == board[7] == board[8] != "*"
-    if row_1 or row_2 or row_3:
-        ongoing_game = False
-    if row_1:
-        return board[0]
-    elif row_2:
-        return board[3]
-    elif row_3:
-        return board[6]
-    else:
-        return None
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
-def check_for_winner():
-    """ Check all possible outcomes for a winner """
-    global winner
-    row_winner = check_rows()
-    column_winner = check_columns()
-    diagonal_winner = check_diagonals()
-    if row_winner:
-        winner = row_winner
-    elif column_winner:
-        winner = column_winner
-    elif diagonal_winner:
-        winner = diagonal_winner
-    else:
-        winner = None
-
-
-def check_if_game_over():
-    """ Check the game for a winner or tie """
-    check_for_winner()
-    check_for_tie()
-
-
-def handle_turn(player):
+def calculate_stock_data(data):
     """
-    Loops through players turns and
-    handles exception errors appropriately
+    Calculate the average stock for each item type, adding 10%
     """
-    print(player + "'s turn.")
-    valid = False
-    position = input("\nChoose a position from 1-9: ")
-    x = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    while not valid:
-        try:
-            if position not in x:
-                raise ValueError
-            else:
-                position = int(position) - 1
-                if board[position] == "*":
-                    valid = True
-                    board[position] = player
-                    display_board()
-                    return
-                else:
-                    raise SpaceTakenError
-        except ValueError:
-            print("\nError: Incorrect Value Please Try Again\n")
-            position = input("\nChoose a position from 1-9: ")
-        except SpaceTakenError:
-            print("\nError: Space Taken, Try Again\n")
-            position = input("\nChoose a position from 1-9: ")
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
-def display_board():
-    """ Displays the game board """
-    print("\n")
-    print("-------------------------------------")
-    print("|  " + board[0] + " | " + board[1] +
-          " | " + board[2] + "             1 | 2 | 3  |")
-    print("|  " + board[3] + " | " + board[4] +
-          " | " + board[5] + "  TicTacToe  4 | 5 | 6  |")
-    print("|  " + board[6] + " | " + board[7] +
-          " | " + board[8] + "             7 | 8 | 9  |")
-    print("-------------------------------------")
-    print("\n")
-
-
-def play_game():
-    """Play a game of tic tac toe, game displays the board,
-    loops the turns whilst game is in session then checks the
-    game is over. If game is ongoing then swaps player until a winner.
+def main():
     """
-    display_board()
-    while ongoing_game:
-        handle_turn(current_player)
-        check_if_game_over()
-        swap_player()
-        global board
-        if winner == "X" or winner == "O":
-            print("<-------- Congratulations " +
-                  winner + ", you win. -------->")
-            play_again()
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
-def start():
-    """ Prompt user for a game of tic tac toe """
-    display_board()
-    print("\n")
-    y_n_prompt()
-
-
-# ------- Execute -------
-
-# Start a game of tic tac toe
-print("\nWelcome to Clayton's TicTacToe Multiplayer Game")
-start()
+print("Welcome to Love Sandwiches Data Automation")
+main()
